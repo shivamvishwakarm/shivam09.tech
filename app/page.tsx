f654cf8610587cdc7b220a8a8881fc80efed25a7
shivam vishwakarma <vishwakarmashivam2003@gmail.com>
2025-08-14 01:04:58 +0530

    doing cleanup"

--- app/page.tsx
+++ app/page.tsx
@@ -10,13 +10,13 @@ import { USER } from "@/data/user";
 export default function Home() {
   return (
     <div className="relative">
-      <div className="max-w-3xl mx-auto absolute inset-0 h-screen w-full z-[-1]">
-        <div className="absolute inset-y-0 left-0 h-full w-[0.5px] bg-gradient-to-b from-stone  via-primary/70 to-primary opacity-50" />
-        <div className="absolute inset-y-0 right-0 h-full w-[0.5px] bg-gradient-to-b from-stone via-primary/70 to-primary opacity-50" />
+      <div className="max-w-3xl mx-auto absolute inset-0 h-screen w-full z-[-1] pointer-events-none">
+        <div className="absolute inset-y-0 left-0 h-full w-[0.5px] bg-gradient-to-b from-stone-500 via-[var(--color-primary)]/70 to-[var(--color-primary)] opacity-50" />
+        <div className="absolute inset-y-0 right-0 h-full w-[0.5px] bg-gradient-to-b from-stone-500 via-[var(--color-primary)]/70 to-[var(--color-primary)] opacity-50" />
       </div>
-      <div className=" absolute inset-0 h-screen w-full z-0 ">
-        <div className=" absolute -top-145 left-119 h-[154.5vh] w-px rounded-full bg-gradient-to-b from-primary  to-stone transform rotate-90 opacity-50" />
 
+      <div className="absolute inset-0 h-screen w-full z-0 pointer-events-none">
+        {/* <div className="absolute -top-[145px] left-[119px] h-[200vh] w-px rounded-full bg-gradient-to-b from-[var(--color-primary)] to-stone-500 transform rotate-90 opacity-50" /> */}
       </div>
 
 
