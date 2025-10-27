b93f1705fe7cc7f73cd744674b11f1eb784c1a9f
shivam vishwakarma <vishwakarmashivam2003@gmail.com>
2025-10-28 01:13:16 +0530

    change dark mode color

--- components/cards/pr-card.tsx
+++ components/cards/pr-card.tsx
@@ -9,7 +9,7 @@ interface PRProps {
 
 const PRCard = (pr: PRProps): React.ReactElement => {
   return (
-    <Link href={pr.link} target='_blank' className='flex items-center gap-2 dark:bg-primary bg-[#f4f4f4] dark:text-neutral-100 px-4 py-3 rounded-md'>
+    <Link href={pr.link} target='_blank' className='flex items-center gap-2 dark:bg-[#1d1d1d] bg-[#f4f4f4] dark:text-neutral-100 px-4 py-3 rounded-md'>
         <CodeMerged size={30} className='text-[#8957e5]'/>
         <p className='text-xl'>{pr.title} </p>
     </Link>
