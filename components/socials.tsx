a1e7386c38a91359738af7bc4cbda58872ef49b8
shivamvishwakarm <vishwakarmashivam2003@gmail.com>
2025-12-28 11:32:11 +0530

    Add: tooltip to the social and resume

--- components/socials.tsx
+++ components/socials.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { USER } from '@/data/user'
+import { SimpleTooltip } from './ui/tooltip'
 
 
 const Socials = () => {
@@ -9,21 +10,22 @@ const Socials = () => {
             <div className='space-x-2 col-span-4 rounded-lg flex md:items-end md:justify-end'>
                 {
                     USER.socialLinks.map((socials) => (
-                        <a
-                            key={socials.name}
-                            href={socials.url}
-                            target="_blank"
-                            rel="noreferrer"
-                            className="group  p-1 rounded-md transition-all duration-200 ease-in-out"
-                        >
-                            <socials.icon
-                            color="#666666"
-                                className="text-neutral-500 shadow-3xl md:size-10 size-8 "
-                                width={35}
-                                height={35}
-                                aria-label={socials.name}
-                            />
-                        </a>
+                        <SimpleTooltip key={socials.name} content={socials.name} side="bottom" sideOffset={-4}>
+                            <a
+                                href={socials.url}
+                                target="_blank"
+                                rel="noreferrer"
+                                className="group  p-1 rounded-md transition-all duration-200 ease-in-out"
+                            >
+                                <socials.icon
+                                    color="#666666"
+                                    className="text-neutral-500 shadow-3xl md:size-10 size-8 "
+                                    width={35}
+                                    height={35}
+                                    aria-label={socials.name}
+                                />
+                            </a>
+                        </SimpleTooltip>
                     ))
 
 
